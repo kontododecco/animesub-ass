02221f0eb5cf60001949b8b49f423224eb1db80b
kontododecco <kontododeco@gmail.com>
2026-02-18 19:21:14 +0100

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,14 @@
+// manifest.js - Definicja manifestu wtyczki
+// NAPRAWIONE: usunięto proxyStreams (niekompatybilne z wieloma wersjami Stremio)
+// resources jako tablica obiektów - bardziej kompatybilne
+module.exports = {
+    id: 'community.animesub.info',
+    version: '1.1.0',
+    name: 'AnimeSub.info Subtitles',
+    description: 'Polskie napisy do anime z animesub.info',
+    logo: 'https://i.imgur.com/qKLYVZx.png',
+    resources: ['subtitles'],
+    types: ['movie', 'series'],
+    idPrefixes: ['tt', 'kitsu'],
+    catalogs: []
+};
