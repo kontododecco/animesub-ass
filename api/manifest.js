5a0ed266518526f56f9d30d7727ece1d5a12f3c7
kontododecco <kontododeco@gmail.com>
2026-02-18 19:04:34 +0100

    Delete api/manifest.js

--- api/manifest.js
+++
@@ -1,9 +0,0 @@
-// api/manifest.js - Endpoint manifestu wtyczki Stremio
-const manifest = require('../manifest');
-
-module.exports = (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'public, max-age=3600');
-    res.status(200).json(manifest);
-};
